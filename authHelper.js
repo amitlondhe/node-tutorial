0f6b1d6c1c10f4c5c559b83e487ae29a09f8af86
amitlondhe <alondhe@icct.com>
2017-12-17 08:06:16 -0500

    Initial Check in
    First checkin

---
+++ authHelper.js
@@ -0,0 +1,58 @@
+var credentials = {
+  client: {
+    id: '197222-b3d4-47c0-9c69-92fa429ae27f',
+    secret: 'qzpZAL47__-ggkiRFMC048=',
+  },
+  auth: {
+    tokenHost: 'https://login.microsoftonline.com',
+    authorizePath: 'common/oauth2/v2.0/authorize',
+    tokenPath: 'common/oauth2/v2.0/token'
+  }
+};
+var oauth2 = require('simple-oauth2').create(credentials);
+
+var redirectUri = 'http://localhost:8000/authorize';
+
+// The scopes the app requires
+// The scopes the app requires
+var scopes = [ 'openid',
+               'offline_access',
+               'User.Read',
+               'Mail.Read' ];
+
+function getAuthUrl() {
+  var returnVal = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  });
+  console.log('Generated auth url: ' + returnVal);
+  return returnVal;
+}
+exports.getAuthUrl = getAuthUrl;
+
+function getTokenFromCode(auth_code, callback, response) {
+  var token;
+  oauth2.authorizationCode.getToken({
+    code: auth_code,
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  }, function (error, result) {
+    if (error) {
+      console.log('Access token error: ', error.message);
+      callback(response, error, null);
+    } else {
+      token = oauth2.accessToken.create(result);
+      console.log('Token created: ', token.token);
+      callback(response, null, token);
+    }
+  });
+}
+
+exports.getTokenFromCode = getTokenFromCode;
+
+function refreshAccessToken(refreshToken, callback) {
+  var tokenObj = oauth2.accessToken.create({refresh_token: refreshToken});
+  tokenObj.refresh(callback);
+}
+
+exports.refreshAccessToken = refreshAccessToken;
